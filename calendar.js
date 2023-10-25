c7a037d1feb6e48424c5280aec10298afe90c22e
Bruno Ferrarezzo <bruno.ferrarezzo@acct.global>
2023-10-25 17:26:55 -0300

    feat: inserting and getting events

--- calendar.js
+++ calendar.js
@@ -23,7 +23,7 @@ const insertEvent = async (event) => {
         });
     
         if (response['status'] == 200 && response['statusText'] === 'OK') {
-            return 1;
+            return response;
         } else {
             return 0;
         }
@@ -32,5 +32,27 @@ const insertEvent = async (event) => {
         return 0;
     }
   };
+  const getEventByDateTime = async (startDateTime,endDateTime) => {
+    try {
+        const response = await calendar.events.list({
+            auth: auth,
+            calendarId: CALENDAR_ID,
+            timeMin: startDateTime, // Data e hora início
+            timeMax: endDateTime, // Data e hora fim
+            maxResults: 1, // Limita a resposta a 1 evento, já que é esperado apenas um evento correspondente
+            timeZone: 'America/Sao_Paulo'
+        });
 
-  module.exports = {insertEvent}
+        if (response.data.items.length > 0) {
+            // Retorna o primeiro evento correspondente encontrado
+            return response.data.items[0];
+        } else {
+            return null; // Retorna null se nenhum evento correspondente for encontrado
+        }
+    } catch (error) {
+        console.log(`Error at getEventByDateTime --> ${error}`);
+        return null; // Retorna null em caso de erro
+    }
+};
+
+  module.exports = {insertEvent, getEventByDateTime}
--- index.js
+++ index.js
@@ -4,19 +4,6 @@ require('dotenv').config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
-const event = {
-  'summary': 'Meu evento 2',
-  'start': {
-      'dateTime': '2023-10-24T15:00:00',
-      'timeZone': 'America/Sao_Paulo',
-  },
-  'end': {
-      'dateTime': '2023-10-24T16:00:00',
-      'timeZone': 'America/Sao_Paulo',
-  },
-}
-
-calendar.insertEvent(event)
 ////////////////////////////////////////////////////////////////////////////
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
@@ -25,8 +12,38 @@ bot.on('message', (msg) => {
   const messageText = msg.text;
 
   if (messageText === '/start') {
-    bot.sendMessage(chatId, 'Olá! Este é o bot do Bruno.');
+    bot.sendMessage(chatId, 'Olá! Sou seu bot de agendamento, por favor insira uma data em que deseja agendar sua consulta no formato dia/mês.');
   } else {
-    bot.sendMessage(chatId, 'Você disse: ' + messageText);
+    const date = messageText.split('/')
+    const startDateTime = `2023-${date[1]}-${date[0]}T03:00:00.000Z`
+    const endDateTime = `2023-${date[1]}-${date[0]}T04:00:00.000Z`
+    const event = {
+      'summary': 'Teste criando evento via bot',
+      'description': `This is the description.`,
+      'start': {
+          'dateTime': startDateTime,
+          'timeZone': 'America/Sao_Paulo',
+      },
+      'end': {
+          'dateTime': endDateTime,
+          'timeZone': 'America/Sao_Paulo',
+      },
+    }
+    calendar.getEventByDateTime(startDateTime,endDateTime).then(
+      response =>{
+        const dateAlreadyExists = (response !== null && response !== undefined)? true : false
+        if(dateAlreadyExists){
+          bot.sendMessage(chatId, 'Infelizmente essa data já está ocupada, por favor escolha outra');
+          return
+        }
+        else{
+          calendar.insertEvent(event).then(response =>{
+            if( response.status === 200){
+              bot.sendMessage(chatId, `Pronto! Você tem uma consulta marcada para o dia ${date[0]}/${date[1]}.`);
+            }
+          })
+        }
+      }
+    )
   }
 });
